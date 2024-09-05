3ee4c75c146f05d86823f2f0885f4c43b7187ba1
Ali Mo'nes <AliIsHomie@outlook.com>
2024-09-05 18:42:16 +0300

    finish extensible message

--- Src/joystick/message.py
+++ Src/joystick/message.py
@@ -1,105 +1,101 @@
 import unittest
+import struct
 
 
 class Message:
 
-    def __init__(self, msg="") -> None:
-        self.throttle = 1500
-        self.yaw = 1500
-        self.forward = 1500
-        self.lateral = 1500
+    def __init__(self, msg=b"") -> None:
+        self.__msg = {
+            "throttle": 1500,
+            "yaw": 1500,
+            "forward": 1500,
+            "lateral": 1500,
+            
+            "gripper_1": False,
+            "gripper_2": False,
+            "light": "0",
+            "rotating_gripper": "O",
 
-        self.gripper_1 = 0
-        self.gripper_2 = 0
-        self.light = "0"
-        self.rotating_gripper = "O"
+            "armed": False,
+            "flight_mode": "M",
+            "joystick_connect": False,
+        }
 
-        self.armed = 0
-        self.flight_mode = "M"
-        self.joystick_connect = 0
+        if msg != b"":
+            index = 0
+            for key in self.__msg.keys():
+                if isinstance(self.__msg[key], int):
+                    self.__msg[key] = int.from_bytes(msg[index : index + 4], "big")
+                    index += 4
+                elif isinstance(self.__msg[key], float):
+                    self.__msg[key] = struct.unpack("d", msg[index : index + 8])[0]
+                    index += 8
+                elif isinstance(self.__msg[key], str):
+                    self.__msg[key] = "".join(
+                        map(chr, msg[index : index + len(self.__msg[key])])
+                    )
+                    index += len(self.__msg[key])
+                elif isinstance(self.__msg[key], bool):
+                    self.__msg[key] = bool.from_bytes(msg[index], "big")
+                    index += 1
+                else:
+                    raise Exception("Unrecognised data type")
 
-        if msg:
-            self.throttle = int(msg[:4])
-            self.yaw = int(msg[4:8])
-            self.forward = int(msg[8:12])
-            self.lateral = int(msg[12:16])
+    def bytes(self) -> bytes:
+        b = b""
+        for key in self.__msg.keys():
+            if isinstance(self.__msg[key], int):
+                b += self.__msg[key].to_bytes(4, "big")
+            elif isinstance(self.__msg[key], float):
+                b += struct.pack("d", self.__msg[key])
+            elif isinstance(self.__msg[key], str):
+                b += self.__msg[key].encode()
+            elif isinstance(self.__msg[key], bool):
+                b += self.__msg[key].to_bytes(1, "big")
+            else:
+                raise Exception("Unrecognised data type")
+        return b
 
-            self.gripper_1 = int(msg[16])
-            self.gripper_2 = int(msg[17])
-            self.light = msg[18]
-            self.rotating_gripper = msg[19]
+    def get_value(self, key):
+        return self.__msg[key]
 
-            self.armed = int(msg[20])
-            self.flight_mode = msg[21]
-            self.joystick_connect = int(msg[22])
-
-    def __str__(self) -> str:
-        return (
-            str(self.throttle)
-            + str(self.yaw)
-            + str(self.forward)
-            + str(self.lateral)
-            + str(self.gripper_1)
-            + str(self.gripper_2)
-            + str(self.light)
-            + str(self.rotating_gripper)
-            + str(self.armed)
-            + str(self.flight_mode)
-            + str(self.joystick_connect)
-        )
+    def set_value(self, key, value) -> None:
+        if (
+            key in self.__msg
+            and type(self.__msg[key]) == type(value)
+            and (
+                type(value) == str
+                and len(value) == len(self.__msg[key])
+                or type(value) != str
+            )
+        ):
+            self.__msg[key] = value
+        else:
+            raise ValueError()
 
     def __eq__(self, value) -> bool:
-        return (
-            self.throttle == value.throttle
-            and self.yaw == value.yaw
-            and self.forward == value.forward
-            and self.lateral == value.lateral
-            and self.gripper_1 == value.gripper_1
-            and self.gripper_2 == value.gripper_2
-            and self.light == value.light
-            and self.rotating_gripper == value.rotating_gripper
-            and self.armed == value.armed
-            and self.flight_mode == value.flight_mode
-            and self.joystick_connect == value.joystick_connect
-        )
+        return self.__msg == value.__msg
 
 
 class Test(unittest.TestCase):
-
-    def test_create_msg_1(self):
+    def test_msg(self):
         encoded_msg = Message()
 
-        encoded_msg.throttle = 1600
-        encoded_msg.yaw = 1600
-        encoded_msg.forward = 1400
-        encoded_msg.lateral = 1300
-        encoded_msg.gripper_1 = 1
-        encoded_msg.gripper_2 = 0
-        encoded_msg.light = "H"
-        encoded_msg.rotating_gripper = "L"
-        encoded_msg.armed = 1
-        encoded_msg.flight_mode = "S"
-        encoded_msg.joystick_connect = 1
+        encoded_msg.set_value("throttle", 1600)
+        encoded_msg.set_value("yaw", 1600)
+        encoded_msg.set_value("forward", 1400)
+        encoded_msg.set_value("lateral", 1200)
 
-        decoded_msg = Message(str(encoded_msg))
-        self.assertTrue(decoded_msg == encoded_msg)
+        encoded_msg.set_value("gripper_1", True)
+        encoded_msg.set_value("gripper_2", False)
+        encoded_msg.set_value("light", "H")
+        encoded_msg.set_value("rotating_gripper", "L")
 
-    def test_create_msg_2(self):
-        encoded_msg = Message()
+        encoded_msg.set_value("armed", True)
+        encoded_msg.set_value("flight_mode", "S")
+        encoded_msg.set_value("joystick_connect", True)
 
-        encoded_msg.throttle = 1100
-        encoded_msg.yaw = 1000
-        encoded_msg.forward = 1100
-        encoded_msg.lateral = 1200
-        encoded_msg.gripper_1 = 1
-        encoded_msg.gripper_2 = 0
-        encoded_msg.light = "0"
-        encoded_msg.rotating_gripper = "R"
-        encoded_msg.armed = 0
-        encoded_msg.flight_mode = "A"
-        encoded_msg.joystick_connect = 0
-
-        decoded_msg = Message(str(encoded_msg))
+        decoded_msg = Message(encoded_msg.bytes())
         self.assertEqual(decoded_msg, encoded_msg)
 
 
