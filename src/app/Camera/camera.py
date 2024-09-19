c1f3962cf65f8125eb20d24ab1fc51a8c0923b5c
mahbahig <mahbahig2@gmail.com>
2024-09-19 16:02:12 +0300

    Oak integrated with GUI

--- src/app/Camera/camera.py
+++ src/app/Camera/camera.py
@@ -21,7 +21,7 @@ class CameraThread(QThread):
             pattern = 1,
             receive_mode = True,
             logging = True,
-            request_timeout = sys.maxsize,
+            # request_timeout = sys.maxsize,
             **options
         )
 
@@ -32,25 +32,25 @@ class CameraThread(QThread):
             i+=1
             if frame is not None:
                 # rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                rotated_frame = cv2.rotate(frame, cv2.ROTATE_180)
+                # rotated_frame = cv2.rotate(frame, cv2.ROTATE_180)
                 # cv2.imshow("Hello",rotated_frame)
-                key = cv2.waitKey(1) 
-                if key == ord(' '):
-                    frame_filename = os.path.join("D:/frames", "frame_{}.jpg".format(len(os.listdir("frames"))))
-                    cv2.imwrite(frame_filename, rotated_frame)
-                    # cv2.imwrite(f"frame {i}",rotated_frame)
-                rgb_image = cv2.cvtColor(rotated_frame, cv2.COLOR_BGR2RGB)
-                cv2.imshow("555555",rotated_frame)
+                # key = cv2.waitKey(1) 
+                # if key == ord(' '):
+                    # frame_filename = os.path.join("D:/frames", "frame_{}.jpg".format(len(os.listdir("frames"))))
+                    # cv2.imwrite(frame_filename, frame)
+                    # cv2.imwrite(f"frame {i}",frame)
+                rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                # cv2.imshow("555555",rotated_frame)
                 self.frame_updated.emit(rgb_image)
 
 
 class Camera:
-    def __init__(self, camera, takephoto):
-        self.camera_label = camera
-        self.takephoto = takephoto
+    def __init__(self, ui):
+        self.ui = ui
+        # self.takephoto = takephoto
         self.camera_thread = CameraThread()
         self.camera_thread.frame_updated.connect(self.update_frame)
-        self.takephoto.clicked.connect(self.save_frame)
+        # self.takephoto.clicked.connect(self.save_frame)
         self.camera_thread.start()
         self.camera_thread.setPriority(QThread.HighestPriority)
 
@@ -58,16 +58,16 @@ class Camera:
         h, w, ch = frame.shape
         Qframe = QImage(frame.data, w, h, ch * w, QImage.Format_RGB888)
         pixmap = QPixmap.fromImage(Qframe)
-        self.camera_label.setPixmap(pixmap)
+        self.ui.camera.setPixmap(pixmap)
     
-    def save_frame(self):
-        pixmap = self.camera_label.pixmap()
-        if pixmap is not None:
-            frame = pixmap.toImage().convertToFormat(QImage.Format_RGB888)
-            frame_data = frame.constBits()
-            frame_data.setsize(frame.byteCount())
-            frame_array = np.array(frame_data).reshape(frame.height(), frame.width(), 3)
-            os.makedirs("frames", exist_ok=True)
-            frame_filename = os.path.join("frames", "frame_{}.jpg".format(len(os.listdir("frames"))))
-            cv2.imwrite(frame_filename, cv2.cvtColor(frame_array, cv2.COLOR_RGB2BGR))
-            print("Frame saved:", frame_filename)
+    # def save_frame(self):
+    #     pixmap = self.ui.camera.pixmap()
+    #     if pixmap is not None:
+    #         frame = pixmap.toImage().convertToFormat(QImage.Format_RGB888)
+    #         frame_data = frame.constBits()
+    #         frame_data.setsize(frame.byteCount())
+    #         frame_array = np.array(frame_data).reshape(frame.height(), frame.width(), 3)
+    #         os.makedirs("frames", exist_ok=True)
+    #         frame_filename = os.path.join("frames", "frame_{}.jpg".format(len(os.listdir("frames"))))
+    #         cv2.imwrite(frame_filename, cv2.cvtColor(frame_array, cv2.COLOR_RGB2BGR))
+    #         print("Frame saved:", frame_filename)
